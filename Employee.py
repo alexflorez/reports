68a842a4a51ac92d5749ffd0f3daa7ce2bcc6538
Alex Florez <alex.josueff@gmail.com>
2014-05-17 09:10:53 -0500

    Employee to save and process daily marks

---
+++ Employee.py
@@ -0,0 +1,30 @@
+__author__ = 'Alex Florez'
+
+
+class Employee():
+    def __init__(self, code, name):
+        self.code = code
+        self.name = name
+        self.marks = []
+
+    def extract_hours(self, rawmark):
+        hours = []
+        marks = rawmark.split(',')
+
+        for m in marks:
+            marca = m[:-3].strip()
+            hours.append(marca)
+        return hours
+
+    # Cada empleado debería tener 4 marcas como max Entrada, Almuerzo, Regreso, Salida
+    # Agregar una hora de almuerzo para los que no regitraron marcacion
+    def add_mark(self, rawmarks):
+        # rawmarks : ['Mar 01/04/2014', '08:07 IN, 13:52 OL, 15:00 IL']
+        try:
+            dia, fecha = rawmarks[0].split(' ')
+            hours = self.extract_hours(rawmarks[1])
+            for hora in hours:
+                marca = fecha + " " + hora
+                self.marks.append(marca)
+        except:
+            return
