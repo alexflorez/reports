53c97d2a37f8a1ee5dab143b5d212762e4726209
Alex Florez <alex.josueff@gmail.com>
2014-06-02 17:04:01 -0500

    Method check_repeated() to correct marks when multiple marks were registered

--- Mark.py
+++ Mark.py
@@ -13,17 +13,19 @@ class Mark():
         return self.mark - other.mark
 
 
-def test_reps(idxs, limit):
-    for i, v in enumerate(idxs):
-        if idxs.count(v) > 1:
-            nidx = idxs[i] + 1
-            if nidx > limit or nidx in idxs:
-                nidx = idxs[i] - 1
-            idxs[i] = nidx
-    return idxs
+def check_reapeted(idxs, marks):
+    tocorrect = {}
+    for i, v in  enumerate(idxs):
+        if v in tocorrect:
+            tocorrect[v].append(marks[i])
+        else:
+            tocorrect[v] = [marks[i]]
+
+    corrected = [min(v) for v in tocorrect.values()]
+    return corrected
 
 
-def correct_marks(marks, ref_mks):
+def find_indexes(marks, ref_mks):
 
     idx = []
     for mk in marks:
@@ -34,31 +36,32 @@ def correct_marks(marks, ref_mks):
             n_mark.append(abs(i.minus(j)))
         elem = n_mark.index(min(n_mark))
         idx.append(elem)
-    idx = test_reps(idx, len(ref_mks) - 1)
     return idx
 
 
 def test():
-    testing = [ ['08:03', '13:52', '14:56', '17:40'],
-                ['07:57', '13:49', '14:51', '18:07'],
-                ['08:10', '14:40', '15:37', '20:47'],
-                ['07:43', '13:51', '14:51', '17:54'],
-                ['08:04', '13:52', '14:50', '18:44'],
-                ['07:59', '14:04', '15:00', '18:06'],
-                ['08:06', '13:47', '15:05', '18:11'],
-                ['07:57', '13:47', '18:02'],
-                ['08:11', '13:24', '14:44', '17:57'],
-                ['07:56', '13:48', '14:51', '17:41'],
-                ['08:00', '13:49', '14:50', '17:52'],
-                ['08:07', '13:35', '14:30', '17:53'],
-                ['12:27', '13:53', '14:50', '17:30'],
-                ['08:08', '15:05', '17:34'] ]
+    # 07:30 a 17:00
+    testing = [ ['07:37', '12:06', '17:07'],
+                ['07:23', '12:50', '17:30'],
+                ['07:24', '12:11', '12:47'],
+                ['07:31', '12:08', '17:00'],
+                ['07:34', '12:03', '13:01'],
+                ['07:21', '12:09', '12:30', '13:05', '17:07'] ]
 
-    testing = [['12:27', '13:53', '14:50', '17:30']]
-    ref_marks = ['08:00', '13:00', '14:00', '17:30']
+    # 13:00 a 21:00
+    #testing = [['12:54']]
+
+    # 05:30 a 13:00
+    #testing = [['04:57'], ['04:50'], ['04:53'], ['04:53', '13:02', '13:16'], ['05:04', '13:06', '13:22'] ]
+
+
+    ref_marks = ['07:30', '12:00', '13:00', '17:00']
+    #ref_marks = ['13:00', '21:00']
+    #ref_marks = ['05:00', '13:00']
 
     for t in testing:
-        idxs = correct_marks(t, ref_marks)
+        idxs = find_indexes(t, ref_marks)
+        t = check_reapeted(idxs, t)
         print(idxs)
         for i, rm in enumerate(ref_marks):
             if i not in idxs:
@@ -67,10 +70,4 @@ def test():
 
 
 if __name__ == '__main__':
-    correct_this = ['14:11', '18:19']
-    ref_marks = ['08:00', '13:00', '14:00', '17:30']
-    #correct_marks(correct_this, ref_marks)
-    #ref = Mark('14:11')
-    #mk = Mark('08:00')
-    #print(ref.minus(mk))
     test()
