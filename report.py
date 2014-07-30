d252f6252f2ab16777a21aa2770501746ec431cf
Alex Florez <alex.josueff@gmail.com>
2014-07-30 12:58:54 -0500

    Template for reports

---
+++ report.py
@@ -0,0 +1,16 @@
+__author__ = 'Alex Florez'
+
+class TemplateReport:
+    def header(self):
+        pass
+
+    def content(self):
+        pass
+
+    def footer(self):
+        pass
+
+    def report(self):
+        self.header()
+        self.content()
+        self.footer()
