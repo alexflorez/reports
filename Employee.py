6cd51fe9a44e77de121ba75fc2fea371fed5d40d
Alex Florez <alex.josueff@gmail.com>
2014-08-06 09:11:32 -0500

    Attribute marks as a dict, key=date, values=[marks]

--- Employee.py
+++ Employee.py
@@ -2,11 +2,12 @@ __author__ = 'Alex Florez'
 
 from collections import OrderedDict
 
+
 class Employee():
     def __init__(self, code, name):
         self.code = code
         self.name = name
-        self.marks = []
+        self.marks = dict()
 
     @staticmethod
     def extract_hours(rawmark):
@@ -19,21 +20,19 @@ class Employee():
         return hours
 
     def add_marks(self, rawmarks):
-        try:
-            list_mark = []
-            dia, fecha = rawmarks[0].split(' ')
-            hours = self.extract_hours(rawmarks[1])
-            for hora in hours:
-                marca = fecha + " " + hora
-                list_mark.append(marca)
+        # rawmarks ['Mié 30/07/2014', '06:54 IN, 13:30 OL, 14:38 IL, 17:54 OT']
+        list_mark = []
+        dia, fecha = rawmarks[0].split(' ')
+        hours = self.extract_hours(rawmarks[1])
+        for hora in hours:
+            marca = fecha + " " + hora
+            list_mark.append(marca)
 
-            for mk in list_mark:
-                fecha, hora = mk.split(' ')
-                if fecha not in self.marks:
-                    self.marks[fecha] = [hora]
-                else:
-                    self.marks[fecha].append(hora)
+        for mk in list_mark:
+            fecha, hora = mk.split(' ')
+            if fecha not in self.marks:
+                self.marks[fecha] = [hora]
+            else:
+                self.marks[fecha].append(hora)
 
-            self.marks = OrderedDict(sorted(self.marks.items()))
-        except:
-            return
+        self.marks = OrderedDict(sorted(self.marks.items()))
