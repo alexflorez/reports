80a144965fa14ea3e88f230d45897b4bff363f26
Alex Florez <alex.josueff@gmail.com>
2014-08-12 10:45:11 -0500

    read() is the interface method for DataReader

--- DataReader.py
+++ DataReader.py
@@ -5,47 +5,52 @@ from Employee import Employee
 
 
 class DataReader():
-    def read_xls(self, filename, sheet=0):
-        """ read data from a xls file """
-        wb = xlrd.open_workbook(filename)
-        ws = wb.sheet_by_index(sheet)
-        return self.prepare_data(ws)
+    def __init__(self, filename):
+        self.source = filename
+        self.data = []
 
-    def prepare_data(self, ws):
-        data = []
-        #num_rows = ws.nrows - 1
+    def open_xls(self, sheet=0):
+        """ open xls file to read data"""
+        wb = xlrd.open_workbook(self.source)
+        ws = wb.sheet_by_index(sheet)
+        return ws
+
+    def read_worksheet(self, ws):
+        num_rows = ws.nrows - 1
         num_cells = ws.ncols - 1
         curr_row = -1
-        curr_cell = -1
-        while curr_cell < num_cells:
-            curr_cell += 1
-            cell_type = ws.cell_type(curr_row, curr_cell)
-            cell_value = ws.cell_value(curr_row, curr_cell)
-            # Cell Types: 0=Empty, 1=Text, 2=Number, 3=Date, 4=Boolean, 5=Error, 6=Blank
-            if cell_type == 1:
-                data.append(cell_value)
+        while curr_row < num_rows:
+            curr_row += 1
+            #row = worksheet.row(curr_row)
+            #print('Row: ', row)
+            curr_cell = -1
+            while curr_cell < num_cells:
+                curr_cell += 1
+                # Cell Types: 0=Empty, 1=Text, 2=Number, 3=Date, 4=Boolean, 5=Error, 6=Blank
+                cell_type = ws.cell_type(curr_row, curr_cell)
+                cell_value = ws.cell_value(curr_row, curr_cell)
+                if cell_type == 1:  #Agregar celdas que contengan Texto
+                    self.data.append(cell_value)
 
-        return self.format_data(data)
-
-    def format_data(self, data):
+    def format_data(self):
         i = 0
         ni = 0
         data_employees = []
 
         while i is not None:
-            i = self.find_code(self, data, i)  # code = data[i]
+            i = self.find_code(self.data, i)  # code = data[i]
             if i is None:
                 break
-            ni = self.find_code(self, data, i + 1)
+            ni = self.find_code(self.data, i + 1)
             if ni is None:  # llego al final
-                ni = len(data)
+                ni = len(self.data)
                 #break
 
-            code = data[i]
-            name = data[i + 1]
+            code = self.data[i]
+            name = self.data[i + 1]
             emp = Employee(code, name)
             # marcas de cada día
-            marks = data[i + 2:ni - 1]
+            marks = self.data[i + 2:ni - 1]
 
             m = 0
             while m < len(marks):
@@ -60,9 +65,17 @@ class DataReader():
         return data_employees
 
     @staticmethod
-    def find_code(self, data, i):
+    def find_code(data, i):
         """ Return the code's position """
         while i < len(data):
             if data[i].isdigit():
                 return i  # No es necesario convertirlo, sólo es para identificar donde inicia el código
             i += 1
+
+    def read(self):
+        ws = self.open_xls()
+        self.read_worksheet(ws)
+        return self.format_data()
+
+    def __repr__(self):
+        return str(self.__dict__)
